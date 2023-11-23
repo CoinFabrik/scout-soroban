adcaff1c0f73dcf9b0e80c039284255d104a0c0d
Jose Garcia Crosta <jgcrosta@gmail.com>
2023-11-23 14:49:28 -0300

    Fix some stuff

--- scout-audit-internal/src/detector/lint_message.rs
+++ scout-audit-internal/src/detector/lint_message.rs
@@ -1,4 +1,4 @@
 pub const DIVIDE_BEFORE_MULTIPLY_LINT_MESSAGE: &str =
     "Division before multiplication might result in a loss of precision";
 pub const UNSAFE_EXPECT_LINT_MESSAGE: &str = "Unsafe usage of `expect`";
-pub const OVERFLOW_CHECK: &str = "Use `overflow-checks = true` in Cargo.toml profile";
+pub const OVERFLOW_CHECK_LINT_MESSAGE: &str = "Use `overflow-checks = true` in Cargo.toml profile";

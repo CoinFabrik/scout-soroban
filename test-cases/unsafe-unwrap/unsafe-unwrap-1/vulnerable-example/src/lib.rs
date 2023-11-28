ba86de9764d3eea5864414db95e6950a382715a9
Facundo Lerena <faculerena@gmail.com>
2023-11-28 13:25:31 -0300

    Changed the test to not bother clippy

--- test-cases/unsafe-unwrap/unsafe-unwrap-1/vulnerable-example/src/lib.rs
+++ test-cases/unsafe-unwrap/unsafe-unwrap-1/vulnerable-example/src/lib.rs
@@ -1,25 +1,32 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl};
+use soroban_sdk::{contract, contractimpl, contracterror};
 
 #[contract]
 pub struct UnsafeUnwrap;
 
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    CustomError = 1,
+}
+
 #[contractimpl]
 impl UnsafeUnwrap {
-    pub fn unwrap_an_empty_thing() -> u64 {
-
-        let a_thing = None;
-        let result = a_thing.unwrap();
-
-        result
-    }
 
     pub fn unwrap_a_thing(n: u64) -> u64 {
 
-        let a_thing = Some(n);
-        let result = a_thing.unwrap();
+        let a_thing = Self::return_a_result(n);
 
-        result
+        a_thing.unwrap()
+    }
+    pub fn return_a_result(n: u64) -> Result<u64, Error> {
+
+        if n == 0 {
+            return Err(Error::CustomError);
+        }
+
+        Ok(n)
     }
 
 }

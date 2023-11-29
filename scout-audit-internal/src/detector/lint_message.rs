fa2ddfe9991a97236dff0f6eee3a872753007c30
Facundo Lerena <faculerena@gmail.com>
2023-11-29 13:35:56 -0300

    Ci preps (#31)
    CI for testing and releasing

--- scout-audit-internal/src/detector/lint_message.rs
+++ scout-audit-internal/src/detector/lint_message.rs
@@ -1,5 +1,5 @@
 pub const DIVIDE_BEFORE_MULTIPLY_LINT_MESSAGE: &str =
     "Division before multiplication might result in a loss of precision";
-pub const UNSAFE_EXPECT_LINT_MESSAGE: &str = "Unsafe usage of `expect`";
 pub const OVERFLOW_CHECK_LINT_MESSAGE: &str = "Use `overflow-checks = true` in Cargo.toml profile";
+pub const UNSAFE_EXPECT_LINT_MESSAGE: &str = "Unsafe usage of `expect`";
 pub const UNSAFE_UNWRAP_MESSAGE: &str = "Unsafe usage of `unwrap`";

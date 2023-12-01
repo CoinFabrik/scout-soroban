ffbf75f698e8c5d0a27db21ec718f14e1238407f
Jose Garcia Crosta <jgcrosta@gmail.com>
2023-12-01 11:49:46 -0300

    Clean remediated test-case imports and move tests

--- test-cases/insufficiently-random-values/insufficiently-random-values-1/remediated-example/src/lib.rs
+++ test-cases/insufficiently-random-values/insufficiently-random-values-1/remediated-example/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-use soroban_sdk::{contract, contracterror, contractimpl, symbol_short, vec, Env, Symbol, Vec};
+use soroban_sdk::{contract, contracterror, contractimpl, Env};
 
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
@@ -25,4 +25,30 @@ impl Contract {
 }
 
 #[cfg(test)]
-mod test;
+mod test {
+    use soroban_sdk::Env;
+
+    use crate::{Contract, ContractClient};
+
+    #[test]
+    fn random_value_sequence() {
+        // Given
+        let env = Env::default();
+        let contract_id = env.register_contract(None, Contract);
+        let client = ContractClient::new(&env, &contract_id);
+
+        // When
+        let first_random_value = client.generate_random_value(&10);
+        let second_random_value = client.generate_random_value(&10);
+        let third_random_value = client.generate_random_value(&10);
+        let fourth_random_value = client.generate_random_value(&10);
+        let fifth_random_value = client.generate_random_value(&10);
+
+        // Then
+        assert_eq!(first_random_value, 6);
+        assert_eq!(second_random_value, 5);
+        assert_eq!(third_random_value, 8);
+        assert_eq!(fourth_random_value, 8);
+        assert_eq!(fifth_random_value, 4);
+    }
+}

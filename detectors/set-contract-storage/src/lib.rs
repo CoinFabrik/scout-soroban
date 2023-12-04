659b62d4565ad8902165a27418b3730c9984d011
Jose Garcia Crosta <jgcrosta@gmail.com>
2023-12-04 17:01:59 -0300

    Edit lint description

--- detectors/set-contract-storage/src/lib.rs
+++ detectors/set-contract-storage/src/lib.rs
@@ -15,7 +15,7 @@ use scout_audit_internal::Detector;
 
 dylint_linting::declare_late_lint! {
     /// ### What it does
-    /// Checks for calls to env::set_contract_storage.
+    /// Checks for calls to env.storage() without a prior call to env.require_auth()
     ///
     /// ### Why is this bad?
     /// Functions using keys as variables without proper access control or input sanitation can allow users to perform changes in arbitrary memory locations.
@@ -25,28 +25,15 @@ dylint_linting::declare_late_lint! {
     ///
     /// ### Example
     /// ```rust
-    /// fn set_contract_storage(
-    ///     &mut self,
-    ///     user_input_key: [u8; 68],
-    ///     user_input_data: u128,
-    /// ) -> Result<()> {
-    ///     env::set_contract_storage(&user_input_key, &user_input_data);
-    ///     Ok(())
+    /// fn set_contract_storage(env: Env) {
+    ///   let _storage = env.storage().instance();
     /// }
     /// ```
     /// Use instead:
     /// ```rust
-    /// fn set_contract_storage(
-    ///     &mut self,
-    ///     user_input_key: [u8; 68],
-    ///     user_input_data: u128,
-    /// ) -> Result<()> {
-    ///     if self.env().caller() == self.owner {
-    ///         env::set_contract_storage(&user_input_key, &user_input_data);
-    ///         Ok(())
-    ///     } else {
-    ///         Err(Error::UserNotOwner)
-    ///     }
+    /// fn set_contract_storage(env: Env, user: Address) {
+    ///   user.require_auth();
+    ///   let _storage = env.storage().instance();
     /// }
     /// ```
     pub SET_STORAGE_WARN,

c44d96e3c0a5c6507420b295c54623d52acdf3a8
Facundo Lerena <faculerena@gmail.com>
2023-12-06 11:18:21 -0300

    dup

--- scout-audit-internal/src/detector/lint_message.rs
+++ scout-audit-internal/src/detector/lint_message.rs
@@ -5,7 +5,6 @@ pub const INSUFFICIENTLY_RANDOM_VALUES_LINT_MESSAGE: &str =
 
 pub const DIVIDE_BEFORE_MULTIPLY_LINT_MESSAGE: &str =
     "Division before multiplication might result in a loss of precision";
-pub const INSUFFICIENTLY_RANDOM_VALUES_LINT_MESSAGE: &str = "Use env.prng() to generate random numbers, and remember that all random numbers are under the control of validators";
 pub const OVERFLOW_CHECK_LINT_MESSAGE: &str = "Use `overflow-checks = true` in Cargo.toml profile";
 pub const UNPROTECTED_UPDATE_CURRENT_CONTRACT_LINT_MESSAGE: &str =
     "This update_current_contract_wasm is called without access control";

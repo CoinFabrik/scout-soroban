b08f02cba2431d86e79f6e73dd1de108c40315f7
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-03-14 09:37:33 -0300

    Add second test case

---
+++ test-cases/unused-return-enum/unused-return-enum-2/remediated-example/src/lib.rs
@@ -0,0 +1,51 @@
+#![no_std]
+
+use soroban_sdk::{contract, contracterror, contractimpl, testutils::arbitrary::arbitrary::Result};
+
+#[contract]
+pub struct UnusedReturnEnum;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    /// An overflow was produced.
+    Overflow = 1,
+}
+
+#[contractimpl]
+impl UnusedReturnEnum {
+    /// Returns the percentage difference between two values.
+    pub fn get_percentage_difference(balance1: u128, balance2: u128) -> Result<u128, Error> {
+        let absolute_difference = value1.abs_diff(value2);
+        let sum = value1 + value2;
+        let result = 100u128
+            .checked_mul(absolute_difference / sum)
+            .ok_or(Error::Overflow)?;
+        Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use soroban_sdk::Env;
+
+    use crate::{UnusedReturnEnum, UnusedReturnEnumClient};
+
+    #[test]
+    fn get_percentage_difference_works() {
+        // Given
+        let env = Env::default();
+        let contract_id = env.register_contract(None, UnusedReturnEnum);
+        let client = UnusedReturnEnumClient::new(&env, &contract_id);
+
+        // When
+        let value1 = 100u128;
+        let value2 = 150u128;
+        let result = client.try_get_percentage_difference(&value1, &value2);
+
+        // Then
+        assert_eq!(result, Ok(Ok(0)));
+    }
+}

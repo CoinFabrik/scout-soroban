92015b018c39f354891eb008b44cb2309fe67568
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-04-09 17:29:49 -0300

    Use function-call-visitor on detector

--- detectors/set-contract-storage/src/lib.rs
+++ detectors/set-contract-storage/src/lib.rs
@@ -18,6 +18,7 @@ use rustc_span::{
     Span, Symbol,
 };
 use scout_audit_clippy_utils::diagnostics::span_lint_and_help;
+use utils::FunctionCallVisitor;
 
 const LINT_MESSAGE: &str = "Abitrary users should not have control over keys because it implies writing any value of left mapping, lazy variable, or the main struct of the contract located in position 0 of the storage";
 
@@ -223,69 +224,5 @@ impl<'a, 'tcx> Visitor<'tcx> for SetStorageWarnVisitor<'a, 'tcx> {
         }
 
         walk_expr(self, expr)
-    }
-}
-
-/// `FunctionCallVisitor` is a visitor struct used to construct a call graph of functions within Rust code.
-///
-/// It records which functions are called by each function, helping in understanding
-/// the relationships between different parts of the codebase.
-struct FunctionCallVisitor<'a, 'tcx> {
-    /// Context from the compiler's late lint pass, providing access to various compiler internals.
-    cx: &'a LateContext<'tcx>,
-
-    /// The `DefId` of the current function being visited. This acts as a key in the `call_graph`.
-    current_fn: DefId,
-
-    /// A mutable reference to a HashMap acting as the call graph. Each function (`DefId`) is mapped
-    /// to a set of functions (`DefId`) it calls.
-    call_graph: &'a mut HashMap<DefId, HashSet<DefId>>,
-}
-
-impl<'a, 'tcx> FunctionCallVisitor<'a, 'tcx> {
-    /// Constructs a new `FunctionCallVisitor`.
-    ///
-    /// # Parameters
-    /// - `cx`: The context from the compiler's late lint pass.
-    /// - `current_fn`: The `DefId` of the current function being analyzed.
-    /// - `call_graph`: A mutable reference to the call graph being constructed.
-    ///
-    /// # Returns
-    /// A new instance of `FunctionCallVisitor`.
-    fn new(
-        cx: &'a LateContext<'tcx>,
-        current_fn: DefId,
-        call_graph: &'a mut HashMap<DefId, HashSet<DefId>>,
-    ) -> Self {
-        FunctionCallVisitor {
-            cx,
-            current_fn,
-            call_graph,
-        }
-    }
-}
-
-impl<'a, 'tcx> Visitor<'tcx> for FunctionCallVisitor<'a, 'tcx> {
-    /// Visits expressions within the code. If the expression is a function call, it is added to the call graph.
-    ///
-    /// # Parameters
-    /// - `expr`: The expression being visited.
-    fn visit_expr(&mut self, expr: &'tcx Expr<'tcx>) {
-        // If the expression is a function call, record it in the call graph.
-        if_chain! {
-            if let ExprKind::Call(call_expr, _) = expr.kind; // Check if the expression is a call.
-            if let ExprKind::Path(ref qpath) = call_expr.kind; // Ensure the call is a path expression.
-            if let Some(def_id) = self.cx.qpath_res(qpath, call_expr.hir_id).opt_def_id(); // Resolve the path to a DefId.
-            then {
-                // Add the called function's DefId to the current function's entry in the call graph.
-                self.call_graph
-                    .entry(self.current_fn)
-                    .or_default()
-                    .insert(def_id);
-            }
-        }
-
-        // Continue walking through the expression tree.
-        walk_expr(self, expr);
     }
 }

7f11566dcacb4535c7e7a58dca9567bd76cdc774
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-04-10 14:26:35 -0300

    Try fix local detectors flag

--- scripts/run-tests.py
+++ scripts/run-tests.py
@@ -52,7 +52,7 @@ def run_integration_tests(detector, root):
             detector,
             "--metadata",
             "--local-detectors",
-            "../detectors",
+            os.path.join("detectors"),
         ],
         root,
     )
@@ -79,7 +79,7 @@ def run_integration_tests(detector, root):
             "--filter",
             detector,
             "--local-detectors",
-            "../detectors",
+            os.path.join("detectors"),
         ],
         root,
     )

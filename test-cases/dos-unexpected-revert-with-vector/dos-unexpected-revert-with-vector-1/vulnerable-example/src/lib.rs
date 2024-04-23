1adce0f54730d41a87a0fa5aa6255f06d7674269
Sofi Azcoaga <sofiaazc3@hotmail.com>
2024-04-23 13:52:28 -0300

    fix ci errors

--- test-cases/dos-unexpected-revert-with-vector/dos-unexpected-revert-with-vector-1/vulnerable-example/src/lib.rs
+++ test-cases/dos-unexpected-revert-with-vector/dos-unexpected-revert-with-vector-1/vulnerable-example/src/lib.rs
@@ -1,17 +1,21 @@
 #![no_std]
 
-use soroban_sdk::{ String, contracterror, contract, contracttype, contractimpl, Symbol, symbol_short, Env, Address, Map, Vec};
+use soroban_sdk::{
+    contract, contracterror, contractimpl, contracttype, symbol_short, Address, Env, Map, String,
+    Symbol, Vec,
+};
 
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 #[repr(u32)]
-pub enum URError { // Unexpected Revert Error 
+pub enum URError {
+    // Unexpected Revert Error
     AccountAlreadyVoted = 1,
-    CandidateAlreadyAdded = 2, 
-    CandidateDoesntExist = 3, 
-    Overflow = 4, 
-    TimestampBeforeCurrentBlock = 5, 
-    VoteEnded = 6 
+    CandidateAlreadyAdded = 2,
+    CandidateDoesntExist = 3,
+    Overflow = 4,
+    TimestampBeforeCurrentBlock = 5,
+    VoteEnded = 6,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -26,34 +30,32 @@ pub struct State {
     vote_timestamp_end: u64,
 }
 
-const STATE: Symbol = symbol_short!("STATE"); 
+const STATE: Symbol = symbol_short!("STATE");
 
 #[contract]
-pub struct UnexpectedRevert; 
+pub struct UnexpectedRevert;
 
 #[contractimpl]
 impl UnexpectedRevert {
-    
     pub fn init(env: Env, end_timestamp: u64) -> Result<State, URError> {
         if end_timestamp <= env.ledger().timestamp() {
-            return Err(URError::TimestampBeforeCurrentBlock); 
+            return Err(URError::TimestampBeforeCurrentBlock);
         }
-        
+
         let zero_string: String = String::from_str(&env, "00000000000000000000000000000000");
-        let zero_addr = Address::from_string(&zero_string);  //CHECK 
+        let zero_addr = Address::from_string(&zero_string); //CHECK
         let state = State {
-            total_votes:0, 
+            total_votes: 0,
             most_voted_candidate: zero_addr,
             candidate_votes: 0,
             candidates: Vec::new(&env),
-            already_voted: Map::new(&env), 
+            already_voted: Map::new(&env),
             votes: Map::new(&env),
             vote_timestamp_end: end_timestamp,
         };
 
-        env.storage().instance().set(&STATE, &state); 
+        env.storage().instance().set(&STATE, &state);
         Ok(state)
-
     }
 
     pub fn add_candidate(env: Env, candidate: Address, caller: Address) -> Result<(), URError> {
@@ -63,31 +65,29 @@ impl UnexpectedRevert {
             return Err(URError::VoteEnded);
         }
         if state.already_voted.contains_key(caller.clone()) {
-            return Err(URError::AccountAlreadyVoted); 
+            return Err(URError::AccountAlreadyVoted);
         } else {
             state.candidates.push_back(candidate.clone());
             state.votes.set(candidate, 0);
             Ok(())
         }
-
-        
-        
     }
 
     pub fn get_votes_for_a_candidate(env: Env, candidate: Address) -> Result<u64, URError> {
-        let state = Self::get_state(env.clone()); 
-        state.votes
+        let state = Self::get_state(env.clone());
+        state
+            .votes
             .get(candidate)
             .ok_or(URError::CandidateDoesntExist)
     }
-    
+
     pub fn most_voted_candidate_votes(env: Env) -> u64 {
         let state = Self::get_state(env);
         state.candidate_votes
     }
 
     pub fn most_voted_candidate(env: Env) -> Address {
-        let state = Self::get_state(env); 
+        let state = Self::get_state(env);
         state.most_voted_candidate
     }
 
@@ -150,8 +150,4 @@ impl UnexpectedRevert {
     pub fn get_state(env: Env) -> State {
         env.storage().instance().get(&STATE).unwrap()
     }
- 
-
 }
-
-

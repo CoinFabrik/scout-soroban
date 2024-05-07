119b5ed671ad9537610f70a014980fc0bf629142
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-05-07 15:25:12 -0300

    Delete rustfmt unused config file and fix rust-fmt ci

--- test-cases/incorrect-exponentiation/incorrect-exponentiation-1/vulnerable-example/src/lib.rs
+++ test-cases/incorrect-exponentiation/incorrect-exponentiation-1/vulnerable-example/src/lib.rs
@@ -14,29 +14,28 @@ pub struct IncorrectExponentiation;
 
 #[contractimpl]
 impl IncorrectExponentiation {
-
-    pub fn init(e: Env){
+    pub fn init(e: Env) {
         e.storage()
-        .instance()
-        .set::<DataKey, u128>(&DataKey::Data, &((255_u128^2) - 1));
+            .instance()
+            .set::<DataKey, u128>(&DataKey::Data, &((255_u128 ^ 2) - 1));
     }
 
     pub fn set_data(e: Env, new_data: u128) {
         e.storage()
-        .instance()
-        .set::<DataKey, u128>(&DataKey::Data, &new_data);
+            .instance()
+            .set::<DataKey, u128>(&DataKey::Data, &new_data);
     }
 
     pub fn exp_data_3(e: Env) -> u128 {
-        let mut data = e.storage()
-        .instance()
-        .get::<DataKey, u128>(&DataKey::Data)
-        .expect("Data not found");
-        
+        let mut data = e
+            .storage()
+            .instance()
+            .get::<DataKey, u128>(&DataKey::Data)
+            .expect("Data not found");
+
         data = data ^ 3;
         data
     }
-
 }
 
 #[cfg(test)]
@@ -47,16 +46,15 @@ mod tests {
 
     #[test]
     fn simple_test() {
-    let env = Env::default();
-    let contract_id = env.register_contract(None, IncorrectExponentiation);
-    let client = IncorrectExponentiationClient::new(&env, &contract_id);
-    env.mock_all_auths();
-    let _user = <Address as testutils::Address>::generate(&env);
+        let env = Env::default();
+        let contract_id = env.register_contract(None, IncorrectExponentiation);
+        let client = IncorrectExponentiationClient::new(&env, &contract_id);
+        env.mock_all_auths();
+        let _user = <Address as testutils::Address>::generate(&env);
 
+        client.init();
+        client.set_data(&10_u128);
 
-    client.init();
-    client.set_data(&10_u128);
-    
-    assert_eq!(client.exp_data_3(), 9);
-}
+        assert_eq!(client.exp_data_3(), 9);
+    }
 }

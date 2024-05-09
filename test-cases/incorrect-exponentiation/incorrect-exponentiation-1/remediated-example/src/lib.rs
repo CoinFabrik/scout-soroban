a74674008d894892aab24439c6ecd93ec4bb8561
tomasavola <tomas.avola@gmail.com>
2024-05-09 11:28:15 -0300

    Update vulnerability

--- test-cases/incorrect-exponentiation/incorrect-exponentiation-1/remediated-example/src/lib.rs
+++ test-cases/incorrect-exponentiation/incorrect-exponentiation-1/remediated-example/src/lib.rs
@@ -13,27 +13,22 @@ pub struct IncorrectExponentiation;
 
 #[contractimpl]
 impl IncorrectExponentiation {
-    pub fn init(e: Env) {
-        e.storage()
-            .instance()
-            .set::<DataKey, u128>(&DataKey::Data, &(255_u128.pow(2) - 1));
-    }
 
     pub fn set_data(e: Env, new_data: u128) {
         e.storage()
-            .instance()
-            .set::<DataKey, u128>(&DataKey::Data, &new_data);
+        .instance()
+        .set::<DataKey, u128>(&DataKey::Data, &new_data);
     }
 
     pub fn exp_data_3(e: Env) -> u128 {
-        let data = e
-            .storage()
-            .instance()
-            .get::<DataKey, u128>(&DataKey::Data)
-            .expect("Data not found");
+        let data = e.storage()
+        .instance()
+        .get::<DataKey, u128>(&DataKey::Data)
+        .expect("Data not found");
 
         data.pow(3)
     }
+
 }
 
 #[cfg(test)]
@@ -44,15 +39,18 @@ mod tests {
 
     #[test]
     fn simple_test() {
-        let env = Env::default();
-        let contract_id = env.register_contract(None, IncorrectExponentiation);
-        let client = IncorrectExponentiationClient::new(&env, &contract_id);
-        env.mock_all_auths();
-        let _user = <Address as testutils::Address>::generate(&env);
+    let env = Env::default();
+    let contract_id = env.register_contract(None, IncorrectExponentiation);
+    let client = IncorrectExponentiationClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let _user = <Address as testutils::Address>::generate(&env);
 
-        client.init();
-        client.set_data(&10_u128);
 
-        assert_eq!(client.exp_data_3(), 1000);
-    }
+
+    client.set_data(&10_u128);
+
+    assert_eq!(client.exp_data_3(), 1000);
 }
+}
+
+
--- test-cases/incorrect-exponentiation/incorrect-exponentiation-1/vulnerable-example/src/lib.rs
+++ test-cases/incorrect-exponentiation/incorrect-exponentiation-1/vulnerable-example/src/lib.rs
@@ -1,5 +1,4 @@
 #![no_std]
-#![allow(clippy::assign_op_pattern)]
 
 use soroban_sdk::{contract, contractimpl, contracttype, Env};
 
@@ -14,28 +13,24 @@ pub struct IncorrectExponentiation;
 
 #[contractimpl]
 impl IncorrectExponentiation {
-    pub fn init(e: Env) {
-        e.storage()
-            .instance()
-            .set::<DataKey, u128>(&DataKey::Data, &((255_u128 ^ 2) - 1));
-    }
 
     pub fn set_data(e: Env, new_data: u128) {
         e.storage()
-            .instance()
-            .set::<DataKey, u128>(&DataKey::Data, &new_data);
+        .instance()
+        .set::<DataKey, u128>(&DataKey::Data, &new_data);
     }
 
+   
     pub fn exp_data_3(e: Env) -> u128 {
-        let mut data = e
-            .storage()
-            .instance()
-            .get::<DataKey, u128>(&DataKey::Data)
-            .expect("Data not found");
-
-        data = data ^ 3;
+        let mut data = e.storage()
+        .instance()
+        .get::<DataKey, u128>(&DataKey::Data)
+        .expect("Data not found");
+        
+        data ^= 3;
         data
     }
+
 }
 
 #[cfg(test)]
@@ -46,15 +41,14 @@ mod tests {
 
     #[test]
     fn simple_test() {
-        let env = Env::default();
-        let contract_id = env.register_contract(None, IncorrectExponentiation);
-        let client = IncorrectExponentiationClient::new(&env, &contract_id);
-        env.mock_all_auths();
-        let _user = <Address as testutils::Address>::generate(&env);
+    let env = Env::default();
+    let contract_id = env.register_contract(None, IncorrectExponentiation);
+    let client = IncorrectExponentiationClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let _user = <Address as testutils::Address>::generate(&env);
 
-        client.init();
-        client.set_data(&10_u128);
-
-        assert_eq!(client.exp_data_3(), 9);
-    }
+    client.set_data(&10_u128);
+    
+    assert_eq!(client.exp_data_3(), 9);
+}
 }

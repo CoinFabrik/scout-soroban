916afcb62cd0cdcb3234b4056aef734ad57ac1cb
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-08-13 15:03:22 -0300

    Edit detector

--- detectors/unnecessary-lint-allow/src/lib.rs
+++ detectors/unnecessary-lint-allow/src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(rustc_private)]
 
 extern crate rustc_ast;
+extern crate rustc_hir;
 extern crate rustc_span;
 
 use clippy_wrappers::span_lint_and_help;
@@ -8,11 +9,12 @@ use if_chain::if_chain;
 use rustc_ast::{
     token::{Delimiter, Token, TokenKind},
     tokenstream::{TokenStream, TokenTree},
-    AttrArgs, AttrKind,
+    visit::FnKind,
+    AttrArgs, AttrKind, Attribute, Expr, HasAttrs, Item, NodeId,
 };
-use rustc_lint::{EarlyContext, EarlyLintPass};
-use rustc_span::Symbol;
-use std::collections::VecDeque;
+use rustc_lint::{EarlyContext, EarlyLintPass, LateLintPass, LintContext};
+use rustc_span::{Span, Symbol};
+use std::collections::{HashMap, VecDeque};
 
 const LINT_MESSAGE: &str = "This `#[scout_allow]` attribute may be unnecessary. Consider removing it if the lint is no longer triggered.";
 
@@ -48,7 +50,6 @@ impl UnnecessaryLintAllow {
                         lint_names.push(ident.to_string());
                     }
                     TokenTree::Delimited(_, _, Delimiter::Parenthesis, inner_stream) => {
-                        // Push the inner stream onto the stack for later processing
                         stack.push_back(inner_stream);
                     }
                     _ => {}
@@ -58,28 +59,34 @@ impl UnnecessaryLintAllow {
 
         lint_names
     }
-}
 
-impl EarlyLintPass for UnnecessaryLintAllow {
-    fn check_attribute(&mut self, cx: &EarlyContext<'_>, attr: &rustc_ast::Attribute) {
-        if_chain! {
-            if !attr.span.from_expansion();
-            if attr.has_name(Symbol::intern("scout_allow"));
-            if let AttrKind::Normal(item) = &attr.kind;
-            if let AttrArgs::Delimited(delimited_args) = &item.item.args;
-            then {
-                let lint_names = self.extract_lint_names(&delimited_args.tokens);
-                for lint_name in lint_names {
-                    span_lint_and_help(
-                        cx,
-                        UNNECESSARY_LINT_ALLOW,
-                        attr.span,
-                        LINT_MESSAGE,
-                        None,
-                        format!("The detector `{}` is no longer triggered. Consider removing the `#[scout_allow({lint_name})]` attribute if the lint is no longer triggered.", lint_name)
-                    );
+    fn check_scout_allow_attrs(&self, cx: &EarlyContext<'_>, attrs: &[Attribute], span: Span) {
+        for attr in attrs {
+            if_chain! {
+                if !attr.span.from_expansion();
+                if attr.has_name(Symbol::intern("scout_allow"));
+                if let AttrKind::Normal(item) = &attr.kind;
+                if let AttrArgs::Delimited(delimited_args) = &item.item.args;
+                then {
+                    let lint_names = self.extract_lint_names(&delimited_args.tokens);
+                    for lint_name in lint_names {
+                        span_lint_and_help(
+                            cx,
+                            UNNECESSARY_LINT_ALLOW,
+                            span,
+                            LINT_MESSAGE,
+                            None,
+                            format!("The detector `{}` is no longer triggered. Consider removing the `#[scout_allow({})]` attribute if the lint is no longer triggered.", lint_name, lint_name)
+                        );
+                    }
                 }
             }
         }
+    }
+}
+
+impl EarlyLintPass for UnnecessaryLintAllow {
+    fn check_item(&mut self, cx: &EarlyContext<'_>, item: &Item) {
+        self.check_scout_allow_attrs(cx, &item.attrs, item.span);
     }
 }

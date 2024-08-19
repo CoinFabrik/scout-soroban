bf96775a8321d6cfb8b770050c05858ca5445752
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-08-19 14:43:47 -0300

    Delete second test-case and edit

--- test-cases/dynamic-instance-storage/dynamic-instance-storage-2/remediated-example/src/lib.rs
+++ test-cases/dynamic-instance-storage/dynamic-instance-storage-2/remediated-example/src/lib.rs
@@ -1,43 +1,44 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Env, String, Symbol};
+use soroban_sdk::{contract, contractimpl, Env, Map, Symbol};
 
 #[contract]
-pub struct StringStorage;
+pub struct MapStorage;
 
 #[contractimpl]
-impl StringStorage {
-    pub fn store_string(e: Env, data: String) {
-        e.storage()
-            .persistent()
-            .set(&Symbol::new(&e, "string_data"), &data);
+impl MapStorage {
+    pub fn store_map(e: Env, data: Map<Symbol, i32>) {
+        data.iter().for_each(|(key, value)| {
+            e.storage().persistent().set(&key, &value);
+        });
     }
 
-    pub fn get_string(e: Env) -> String {
-        e.storage()
-            .persistent()
-            .get(&Symbol::new(&e, "string_data"))
-            .unwrap()
+    pub fn get_key(e: Env, key: Symbol) -> i32 {
+        e.storage().persistent().get(&key).unwrap()
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use soroban_sdk::Env;
+    use soroban_sdk::{symbol_short, Env, Map};
 
     #[test]
-    fn test_string_storage() {
+    fn test_map_storage() {
         // Given
         let env = Env::default();
-        let contract_id = env.register_contract(None, StringStorage);
-        let client = StringStorageClient::new(&env, &contract_id);
+        let contract_id = env.register_contract(None, MapStorage);
+        let client = MapStorageClient::new(&env, &contract_id);
 
         // When
-        let test_string = String::from_str(&env, "Hello, Soroban!");
-        client.store_string(&test_string);
+        let mut test_map = Map::new(&env);
+        test_map.set(symbol_short!("key1"), 1);
+        test_map.set(symbol_short!("key2"), 2);
+        client.store_map(&test_map);
 
         // Then
-        let retrieved_string = client.get_string();
-        assert_eq!(test_string, retrieved_string);
+        let key1 = client.get_key(&symbol_short!("key1"));
+        let key2 = client.get_key(&symbol_short!("key2"));
+        assert_eq!(test_map.get(symbol_short!("key1")).unwrap(), key1);
+        assert_eq!(test_map.get(symbol_short!("key2")).unwrap(), key2);
     }
 }

cddf66d93659915043a6b506de6677ca14662f75
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-08-23 19:06:50 -0300

    Revert unsafe-unwrap changes

--- detectors/unsafe-unwrap/src/lib.rs
+++ detectors/unsafe-unwrap/src/lib.rs
@@ -1,22 +1,23 @@
 #![feature(rustc_private)]
 #![allow(clippy::enum_variant_names)]
 
+extern crate rustc_ast;
 extern crate rustc_hir;
 extern crate rustc_span;
 
-use clippy_utils::higher::IfOrIfLet;
+use std::{collections::HashSet, hash::Hash};
+
+use clippy_utils::higher;
 use clippy_wrappers::span_lint_and_help;
 use if_chain::if_chain;
 use rustc_hir::{
     def::Res,
     def_id::LocalDefId,
     intravisit::{walk_expr, FnKind, Visitor},
-    BinOpKind, Body, Expr, ExprKind, FnDecl, HirId, LetStmt, PathSegment, QPath, UnOp,
+    BinOpKind, Body, Expr, ExprKind, FnDecl, HirId, PathSegment, QPath, UnOp,
 };
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_span::{sym, Span, Symbol};
-use std::{collections::HashSet, hash::Hash};
-use utils::{fn_returns, get_node_type_opt, match_type_to_str};
 
 const LINT_MESSAGE: &str = "Unsafe usage of `unwrap`";
 const PANIC_INDUCING_FUNCTIONS: [&str; 2] = ["panic", "bail"];
@@ -62,13 +63,6 @@ dylint_linting::declare_late_lint! {
         help: "https://coinfabrik.github.io/scout-soroban/docs/detectors/unsafe-unwrap",
         vulnerability_class: "Validations and error handling",
     }
-}
-
-// Enum to represent the type being unwrapped
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-enum UnwrapType {
-    Option,
-    Result,
 }
 
 /// Represents the type of check performed on method call expressions to determine their safety or behavior.
@@ -138,7 +132,7 @@ impl ConditionalChecker {
     fn from_expression(condition: &Expr<'_>) -> HashSet<Self> {
         match condition.kind {
             // Single `not` expressions are supported
-            ExprKind::Unary(UnOp::Not, condition) => Self::handle_condition(condition, true),
+            ExprKind::Unary(op, condition) => Self::handle_condition(condition, op == UnOp::Not),
             // Multiple `or` expressions are supported
             ExprKind::Binary(op, left_condition, right_condition) if op.node == BinOpKind::Or => {
                 let mut result = Self::from_expression(left_condition);
@@ -156,33 +150,9 @@ struct UnsafeUnwrapVisitor<'a, 'tcx> {
     cx: &'a LateContext<'tcx>,
     conditional_checker: HashSet<ConditionalChecker>,
     checked_exprs: HashSet<HirId>,
-    returns_result_or_option: bool,
 }
 
 impl UnsafeUnwrapVisitor<'_, '_> {
-    fn get_help_message(&self, unwrap_type: UnwrapType) -> &'static str {
-        match (self.returns_result_or_option, unwrap_type) {
-            (true, UnwrapType::Option) => "Consider using `ok_or` to convert Option to Result",
-            (true, UnwrapType::Result) => "Consider using the `?` operator for error propagation",
-            (false, UnwrapType::Option) => {
-                "Consider pattern matching or using `if let` instead of `unwrap`"
-            }
-            (false, UnwrapType::Result) => {
-                "Consider handling the error case explicitly or using `if let` instead of `unwrap`"
-            }
-        }
-    }
-
-    fn determine_unwrap_type(&self, receiver: &Expr<'_>) -> UnwrapType {
-        let type_opt = get_node_type_opt(self.cx, &receiver.hir_id);
-        if let Some(type_) = type_opt {
-            if match_type_to_str(self.cx, type_, "Result") {
-                return UnwrapType::Result;
-            }
-        }
-        UnwrapType::Option
-    }
-
     fn is_panic_inducing_call(&self, func: &Expr<'_>) -> bool {
         if let ExprKind::Path(QPath::Resolved(_, path)) = &func.kind {
             return PANIC_INDUCING_FUNCTIONS.iter().any(|&func| {
@@ -205,23 +175,21 @@ impl UnsafeUnwrapVisitor<'_, '_> {
         None
     }
 
-    fn update_conditional_checker(
-        &mut self,
-        conditional_checkers: &HashSet<ConditionalChecker>,
-        set: bool,
-    ) {
+    fn set_conditional_checker(&mut self, conditional_checkers: &HashSet<ConditionalChecker>) {
         for checker in conditional_checkers {
-            if set {
-                self.conditional_checker.insert(*checker);
-                if checker.check_type.is_safe_to_unwrap() {
-                    self.checked_exprs.insert(checker.checked_expr_hir_id);
-                }
-            } else {
-                if checker.check_type.is_safe_to_unwrap() {
-                    self.checked_exprs.remove(&checker.checked_expr_hir_id);
-                }
-                self.conditional_checker.remove(checker);
+            self.conditional_checker.insert(*checker);
+            if checker.check_type.is_safe_to_unwrap() {
+                self.checked_exprs.insert(checker.checked_expr_hir_id);
             }
+        }
+    }
+
+    fn reset_conditional_checker(&mut self, conditional_checkers: HashSet<ConditionalChecker>) {
+        for checker in conditional_checkers {
+            if checker.check_type.is_safe_to_unwrap() {
+                self.checked_exprs.remove(&checker.checked_expr_hir_id);
+            }
+            self.conditional_checker.remove(&checker);
         }
     }
 
@@ -285,20 +253,18 @@ impl UnsafeUnwrapVisitor<'_, '_> {
 }
 
 impl<'a, 'tcx> Visitor<'tcx> for UnsafeUnwrapVisitor<'a, 'tcx> {
-    fn visit_local(&mut self, local: &'tcx LetStmt<'tcx>) -> Self::Result {
+    fn visit_local(&mut self, local: &'tcx rustc_hir::LetStmt<'tcx>) -> Self::Result {
         if let Some(init) = local.init {
             match init.kind {
                 ExprKind::MethodCall(path_segment, receiver, args, _) => {
                     if self.is_method_call_unsafe(path_segment, receiver, args) {
-                        let unwrap_type = self.determine_unwrap_type(receiver);
-                        let help_message = self.get_help_message(unwrap_type);
                         span_lint_and_help(
                             self.cx,
                             UNSAFE_UNWRAP,
                             local.span,
                             LINT_MESSAGE,
                             None,
-                            help_message,
+                            "Please, use a custom error instead of `unwrap`",
                         );
                     }
                 }
@@ -334,17 +300,17 @@ impl<'a, 'tcx> Visitor<'tcx> for UnsafeUnwrapVisitor<'a, 'tcx> {
         }
 
         // Find `if` or `if let` expressions
-        if let Some(IfOrIfLet {
+        if let Some(higher::IfOrIfLet {
             cond,
             then: if_expr,
             r#else: _,
-        }) = IfOrIfLet::hir(expr)
+        }) = higher::IfOrIfLet::hir(expr)
         {
             // If we are interested in the condition (if it is a CheckType) we traverse the body.
             let conditional_checker = ConditionalChecker::from_expression(cond);
-            self.update_conditional_checker(&conditional_checker, true);
+            self.set_conditional_checker(&conditional_checker);
             walk_expr(self, if_expr);
-            self.update_conditional_checker(&conditional_checker, false);
+            self.reset_conditional_checker(conditional_checker);
             return;
         }
 
@@ -354,18 +320,16 @@ impl<'a, 'tcx> Visitor<'tcx> for UnsafeUnwrapVisitor<'a, 'tcx> {
             if path_segment.ident.name == sym::unwrap;
             then {
                 let receiver_hir_id = self.get_unwrap_info(receiver);
-                // If the receiver is `None` or `Err`, then we assume that the `unwrap` is unsafe
+                // If the receiver is `None`, then we asume that the `unwrap` is unsafe
                 let is_checked_safe = receiver_hir_id.map_or(false, |id| self.checked_exprs.contains(&id));
                 if !is_checked_safe {
-                    let unwrap_type = self.determine_unwrap_type(receiver);
-                    let help_message = self.get_help_message(unwrap_type);
                     span_lint_and_help(
                         self.cx,
                         UNSAFE_UNWRAP,
                         expr.span,
                         LINT_MESSAGE,
                         None,
-                        help_message,
+                        "Please, use a custom error instead of `unwrap`",
                     );
                 }
              }
@@ -380,7 +344,7 @@ impl<'tcx> LateLintPass<'tcx> for UnsafeUnwrap {
         &mut self,
         cx: &LateContext<'tcx>,
         _: FnKind<'tcx>,
-        fn_decl: &'tcx FnDecl<'tcx>,
+        _: &'tcx FnDecl<'tcx>,
         body: &'tcx Body<'tcx>,
         span: Span,
         _: LocalDefId,
@@ -394,8 +358,6 @@ impl<'tcx> LateLintPass<'tcx> for UnsafeUnwrap {
             cx,
             checked_exprs: HashSet::new(),
             conditional_checker: HashSet::new(),
-            returns_result_or_option: fn_returns(fn_decl, sym::Result)
-                || fn_returns(fn_decl, sym::Option),
         };
 
         walk_expr(&mut visitor, body.value);

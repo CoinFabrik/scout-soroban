6839388e31fd48cb99f993b006ad54da37a48b0d
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-08-30 19:46:47 -0300

    Use clippy_wrappers instead of clippy_utils

--- detectors/integer-overflow-or-underflow/src/lib.rs
+++ detectors/integer-overflow-or-underflow/src/lib.rs
@@ -3,7 +3,7 @@
 extern crate rustc_hir;
 extern crate rustc_span;
 
-use clippy_utils::diagnostics::span_lint_and_help;
+use clippy_wrappers::span_lint_and_help;
 use rustc_hir::{
     intravisit::{walk_expr, FnKind, Visitor},
     BinOpKind, Body, Expr, ExprKind, FnDecl, UnOp,
